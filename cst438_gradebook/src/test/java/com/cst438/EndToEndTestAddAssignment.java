8a5e99437982a8d5e6463d61dceecec230194356
nyra-23 <nyrausi@gmail.com>
2022-10-31 17:14:32 -0700

    Added test for invalid addAssignment cases (invalid name, invalid due date, and invalid course id).

--- cst438_gradebook/src/test/java/com/cst438/EndToEndTestAddAssignment.java
+++ cst438_gradebook/src/test/java/com/cst438/EndToEndTestAddAssignment.java
@@ -28,11 +28,15 @@ public class EndToEndTestAddAssignment {
 
 	public static final String URL = "http://localhost:3000";
 	public static final String TEST_INSTRUCTOR_EMAIL = "dwisneski@csumb.edu";
-	public static final int SLEEP_DURATION = 1000; // 1 second.
+	public static final int SLEEP_DURATION = 2500; // 2.5 seconds
+	
 	public static final String TEST_ASSIGNMENT_NAME = "Test Assignment";
 	//set assignment due date to 24 hours ago
 	public static final String TEST_DUE_DATE = (new java.sql.Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000)).toString();
 	public static final int TEST_COURSE_ID = 123456;
+	
+	public static final String TEST_INVALID_DUE_DATE = "2022-";
+	public static final int TEST_INVALID_COURSE_ID = -1;
 
 	@Autowired
 	CourseRepository courseRepository;
@@ -115,6 +119,138 @@ public class EndToEndTestAddAssignment {
 
         	for (Assignment assignment : assignmentRepository.findAll()) {
         		if (assignment.getName().equals(TEST_ASSIGNMENT_NAME)) {
+        			testAssignment = assignment;
+        			break;
+        		}
+        	}
+        	
+	        if (testAssignment != null) {
+	        	assignmentRepository.delete(testAssignment);
+	        }
+
+			driver.quit();
+		}
+
+	}
+	
+	@Test
+	public void invalidAddAssignmentTest() throws Exception {
+		
+		// deletes any previously existing test assignments in the database
+		Assignment x = null;
+        do {
+        	x = null;
+        	for (Assignment assignment : assignmentRepository.findAll()) {
+        		if (assignment.getName().equals(TEST_ASSIGNMENT_NAME) || assignment.getName().equals("")) {
+        			x = assignment;
+        			break;
+        		}
+        	}
+            if (x != null)
+            	assignmentRepository.delete(x);
+        } while (x != null);
+					
+		// set the driver location and start driver
+		//@formatter:off
+		// browser	property name 				Java Driver Class
+		// edge 	webdriver.edge.driver 		EdgeDriver
+		// FireFox 	webdriver.firefox.driver 	FirefoxDriver
+		// IE 		webdriver.ie.driver 		InternetExplorerDriver
+		//@formatter:on
+		
+		/*
+		 * initialize the WebDriver and get the home page. 
+		 */
+
+		System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
+		WebDriver driver = new ChromeDriver();
+		// Puts an Implicit wait for 10 seconds before throwing exception
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get(URL);
+		Thread.sleep(SLEEP_DURATION);
+		
+
+		try {
+			
+			driver.findElement(By.id("Add")).click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			// test for invalid assignment name (blank name)
+			driver.findElement(By.xpath("//input[@name='dueDate']")).sendKeys(TEST_DUE_DATE);
+			driver.findElement(By.xpath("//input[@name='courseId']")).sendKeys(Integer.toString(TEST_COURSE_ID));
+			driver.findElement(By.id("Submit")).click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			// verify that the test assignment is not in the database
+			boolean notFoundTestAssignment = true;
+			for (Assignment assignment : assignmentRepository.findAll()) {
+        		if (assignment.getName().equals("")) {
+        			notFoundTestAssignment = false;
+        			break;
+        		}
+        	}
+			assertTrue(notFoundTestAssignment, " Found invalid assignment with blank name in database. Please try again. ");
+			
+			// verify that the failed toast message pops up
+			String toastText = driver.findElement(By.cssSelector(".Toastify__toast-body div:nth-child(2)")).getText();
+			assertEquals("Adding assignment failed", toastText);
+			
+			// verify that message prompting user to enter a name pops up
+			String msgText = driver.findElement(By.id("Msg")).getText();
+			assertEquals("Please enter a name for the assignment.", msgText);
+			
+			// test for invalid assignment due date
+			driver.findElement(By.xpath("//input[@name='name']")).sendKeys(TEST_ASSIGNMENT_NAME);
+			driver.findElement(By.xpath("//input[@name='dueDate']")).sendKeys(Keys.chord(Keys.CONTROL, "a"), TEST_INVALID_DUE_DATE);
+			driver.findElement(By.id("Submit")).click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			// verify that the test assignment is not in the database
+			notFoundTestAssignment = true;
+			for (Assignment assignment : assignmentRepository.findAll()) {
+        		if (assignment.getName().equals(TEST_ASSIGNMENT_NAME)) {
+        			notFoundTestAssignment = false;
+        			break;
+        		}
+        	}
+			assertTrue(notFoundTestAssignment, " Found invalid assignment with blank name in database. Please try again. ");
+			
+			// verify that the failed toast message pops up
+			toastText = driver.findElement(By.cssSelector(".Toastify__toast-body div:nth-child(2)")).getText();
+			assertEquals("Adding assignment failed", toastText);
+			
+			// test for invalid assignment course id
+			driver.findElement(By.xpath("//input[@name='dueDate']")).sendKeys(Keys.chord(Keys.CONTROL, "a"), TEST_DUE_DATE);
+			driver.findElement(By.xpath("//input[@name='courseId']")).sendKeys(Keys.chord(Keys.CONTROL, "a"),Integer.toString(TEST_INVALID_COURSE_ID));
+			driver.findElement(By.id("Submit")).click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			// verify that the test assignment is not in the database
+			notFoundTestAssignment = true;
+			for (Assignment assignment : assignmentRepository.findAll()) {
+        		if (assignment.getName().equals(TEST_ASSIGNMENT_NAME)) {
+        			notFoundTestAssignment = false;
+        			break;
+        		}
+        	}
+			assertTrue(notFoundTestAssignment, " Found invalid assignment with blank name in database. Please try again. ");
+			
+			// verify that the failed toast message pops up
+			toastText = driver.findElement(By.cssSelector(".Toastify__toast-body div:nth-child(2)")).getText();
+			assertEquals("Adding assignment failed", toastText);			
+
+		} catch (Exception ex) {
+			throw ex;
+		} finally {
+
+			/*
+			 *  in case, clean up database so the test is repeatable.
+			 */
+			Assignment testAssignment = null;
+
+        	for (Assignment assignment : assignmentRepository.findAll()) {
+        		if (assignment.getName().equals(TEST_ASSIGNMENT_NAME) || assignment.getName().equals("")) {
         			testAssignment = assignment;
         			break;
         		}
